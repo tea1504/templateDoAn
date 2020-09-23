3a92c62f25593d775c207696dd3c86927cb3e290
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-09-23 10:41:47 +0700

    Tạo Mới

---
+++ template-index/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once(__DIR__.'/style.css'); ?>
+    <link rel="stylesheet" href="/templateDoAn/vendor/bootstrap/css/bootstrap.min.css">
+    <title>Template index</title>
+</head>
+<body>
+    <div class="container-fluld">
+        <?php include_once(__DIR__.'/../layout/navbar.php'); ?>
+    </div>
+    <?php include_once(__DIR__.'/../layout/footer.php'); ?>
+    <script src="/templateDoAn/vendor/jquery/jquery.js"></script>
+    <script src="/templateDoAn/vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
