96027a3c1e8ec4964dbe99967fd50f1154aa9a57
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-09-27 22:59:53 +0700

    Thêm phần validation cho trang đăng ký

--- template-index/dangky.php
+++ template-index/dangky.php
@@ -24,8 +24,8 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-12 shadow-lg my-md-5 my-sm-0 px-0">
-                <h1 class="myfont text-center py-5">Đăng ký tài khoản</h1>
+            <div class="col-md-12 shadow-lg my-md-5 my-sm-0 px-0" style="border-radius: 5px;">
+                <h1 class="myfont text-center py-5" style="border-radius: 5px 5px 0 0;">Đăng ký tài khoản</h1>
                 <form action="" method="post" id="frm_dang_ky" name="frm_dang_ky" class="px-md-5 px-sm-0">
                     <fieldset class="mt-md-5 mt-sm-0">
                         <legend class="myfont mb-0">Phần thông tin cơ bản</legend>
@@ -33,10 +33,12 @@
                         <div class="form-group">
                             <label for="ten">Họ và tên: <span class="text-danger font-weight-bold">*</span></label>
                             <input type="text" name="ten" id="ten" class="form-control">
+                            <div class="valid-feedback"> Đã nhập.</div>
                         </div>
                         <div class="form-group">
                             <label for="ngaysinh">Ngày sinh: <span class="text-danger font-weight-bold">*</span></label>
                             <input type="date" id="ngaysinh" name="ngaysinh" class="form-control">
+                            <div class="valid-feedback"> Đã nhập.</div>
                         </div>
                         <fieldset class="form-group">
                             <div class="row">
@@ -64,10 +66,12 @@
                         <div class="form-group">
                             <label for="dienthoai">Số điện thoại: <span class="text-danger font-weight-bold">*</span></label>
                             <input type="tel" name="dienthoai" id="dienthoai" class="form-control">
+                            <div class="valid-feedback"> Đã nhập.</div>
                         </div>
                         <div class="form-group">
                             <label for="diachi">Địa chỉ: <span class="text-danger font-weight-bold">*</span></label>
                             <input type="text" name="diachi" id="diachi" class="form-control">
+                            <div class="valid-feedback"> Đã nhập.</div>
                         </div>
                     </fieldset>
                     <fieldset class="mt-md-5 mt-sm-0">
@@ -76,21 +80,24 @@
                         <div class="form-group">
                             <label for="tendangnhap">Tên đăng nhập: <span class="text-danger font-weight-bold">*</span></label>
                             <input type="text" name="tendangnhap" id="tendangnhap" class="form-control">
+                            <div class="valid-feedback"> Đã nhập.</div>
                         </div>
                         <div class="form-group">
                             <label for="matkhau">Mật khẩu: <span class="text-danger font-weight-bold">*</span></label>
                             <input type="password" name="matkhau" id="matkhau" class="form-control">
+                            <div class="valid-feedback"> Đã nhập.</div>
                         </div>
                         <div class="form-group">
                             <label for="nhaplaimatkhau">Nhập lại mật khẩu: <span class="text-danger font-weight-bold">*</span></label>
                             <input type="password" name="nhaplaimatkhau" id="nhaplaimatkhau" class="form-control">
+                            <div class="valid-feedback"> Đã nhập.</div>
                         </div>
                     </fieldset>
                     <div class="form-group text-center">
                         <button class="btn btn-outline-danger">Đăng ký</button>
                     </div>
                 </form>
-                <h1 class="py-5 m-0"></h1>
+                <h1 class="py-5 m-0" style="border-radius: 0 0 5px 5px;"></h1>
             </div>
         </div>
     </div>
@@ -101,6 +108,55 @@
     <script src="/templateDoAn/vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="/templateDoAn/vendor/jquery-validation/jquery.validate.min.js"></script>
     <script src="/templateDoAn/vendor/jquery-validation/localization/messages_vi.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#frm_dang_ky').validate({
+                rules: {
+                    ten: {
+                        required: true
+                    },
+                    ngaysinh: {
+                        required: true,
+                    },
+                    gioitinh: {
+                        required: true,
+                    },
+                    dienthoai: {
+                        required: true,
+                    },
+                    diachi: {
+                        required: true,
+                    },
+                    tendangnhap: {
+                        required: true,
+                    },
+                    matkhau: {
+                        required: true,
+                    },
+                    nhaplaimatkhau: {
+                        required: true,
+                        equalTo: "#matkhau"
+                    },
+                },
+                errorElement: "em",
+                errorPlacement: function(error, element) {
+                    error.addClass("invalid-feedback");
+                    if (element.prop("type") === "checkbox") {
+                        error.insertAfter(element.parent("label"));
+                    } else {
+                        error.insertAfter(element);
+                    }
+                },
+                success: function(label, element) {},
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).addClass("is-valid").removeClass("is-invalid");
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
