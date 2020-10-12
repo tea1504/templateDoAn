08d47396987aafd4c186eb57660d508d45d37aff
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2020-10-12 21:03:29 +0700

    Thêm sản phẩm giảm giá

--- test-Database/index.php
+++ test-Database/index.php
@@ -148,6 +148,7 @@ include_once(__DIR__ . '/dbconnect.php');
                 </div>
             <?php endforeach; ?>
         </div>
+        <!-- Sản phẩm mới  -->
         <?php
         $sqlDanhSachSanPhamMoi = " SELECT sp.sp_id, sp.sp_ten, sp.sp_gia, sp.sp_giacu, sp.sp_avt_tenfile, hsp.hsp_tenfile AS hsp_tenfile,AVG(bl.bl_sao) AS sao FROM sanpham AS sp LEFT JOIN hinhsanpham AS hsp ON sp.sp_id = hsp.sanpham_sp_id LEFT JOIN binhluan AS bl ON sp.sp_id = bl.sanpham_sp_id GROUP BY sp.sp_id ORDER BY sp.sp_ngaycapnhat DESC LIMIT 0, 4;";
         $resultDanhSachSanPhamMoi = mysqli_query($conn, $sqlDanhSachSanPhamMoi);
@@ -212,6 +213,7 @@ include_once(__DIR__ . '/dbconnect.php');
                 </div>
             <?php endforeach; ?>
         </div>
+        <!-- Yêu thích  -->
         <?php
         $sqlDanhSachSanPhamMoi = " SELECT sp.sp_id, sp.sp_ten, sp.sp_gia, sp.sp_giacu, sp.sp_avt_tenfile, hsp.hsp_tenfile AS hsp_tenfile,AVG(bl.bl_sao) AS sao FROM sanpham AS sp LEFT JOIN hinhsanpham AS hsp ON sp.sp_id = hsp.sanpham_sp_id LEFT JOIN binhluan AS bl ON sp.sp_id = bl.sanpham_sp_id GROUP BY sp.sp_id ORDER BY sp.sp_yeuthich DESC LIMIT 0, 4;";
         $resultDanhSachSanPhamMoi = mysqli_query($conn, $sqlDanhSachSanPhamMoi);
@@ -234,6 +236,7 @@ include_once(__DIR__ . '/dbconnect.php');
                 <div class="col py-3">
                     <div class="card my-card">
                         <a href="chitiet.php?sp_id=<?= $sp['sp_id'] ?>">
+                            <!-- Ảnh  -->
                             <div class="my-box-card-img">
                                 <!-- Ảnh đại diện -->
                                 <?php if (!file_exists('../anh-do-an/' . $sp['sp_avt_tenfile'])) : ?>
@@ -247,6 +250,7 @@ include_once(__DIR__ . '/dbconnect.php');
                                 <?php else : ?>
                                     <img src="/templatedoan/anh-do-an/<?= $sp['hsp_tenfile'] ?>" alt="<?= $sp['sp_ten'] ?>" class="card-img-top my-card-img img-hide">
                                 <?php endif; ?>
+                                <!-- Đánh giá -->
                                 <div class="text-danger danh_gia">
                                     <?php for ($i = 1; $i <= floor($sp['sao']); $i++) : ?>
                                         <i class="fa fa-star" aria-hidden="true"></i>
@@ -260,16 +264,91 @@ include_once(__DIR__ . '/dbconnect.php');
                                 </div>
                             </div>
                         </a>
+                        <!-- Phần thân -->
                         <div class="card-body px-0">
+                            <!-- Tên  -->
                             <a href="chitiet.php?sp_id=<?= $sp['sp_id'] ?>" class="card-title my-card-title font-weight-bold">
                                 <?= $sp['sp_ten'] ?>
                             </a>
+                            <!-- Giá  -->
                             <h5 class="my-3">
                                 <?php if ($sp['sp_giacu'] != "0") : ?>
                                     <span class="text-secondary"><s><?= $sp['sp_giacu'] ?></s>
                                     <?php endif; ?>
                                     </span> <span class="text-danger"><?= $sp['sp_gia'] ?> đ</span>
                             </h5>
+                            <!-- Đặt mua  -->
+                            <button class="btn myfont text-danger btn-add btn-mua" data-sp_id="<?= $sp['sp_id'] ?>">Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <!-- Yêu thích  -->
+        <?php
+        $sqlDanhSachSanPhamGiamGia = " SELECT sp.sp_id, sp.sp_ten, sp.sp_gia, sp.sp_giacu, sp.sp_avt_tenfile, hsp.hsp_tenfile AS hsp_tenfile,AVG(bl.bl_sao) AS sao FROM sanpham AS sp LEFT JOIN hinhsanpham AS hsp ON sp.sp_id = hsp.sanpham_sp_id LEFT JOIN binhluan AS bl ON sp.sp_id = bl.sanpham_sp_id WHERE sp.sp_giacu > 0 GROUP BY sp.sp_id ORDER BY (sp.sp_giacu - sp.sp_gia) DESC LIMIT 0,4;";
+        $resultDanhSachSanPhamGiamGia = mysqli_query($conn, $sqlDanhSachSanPhamGiamGia);
+        $dataDanhSachSanPhamGiamGia = [];
+        while ($row = mysqli_fetch_array($resultDanhSachSanPhamGiamGia, MYSQLI_ASSOC)) {
+            $dataDanhSachSanPhamGiamGia[] = array(
+                'sp_id' => $row['sp_id'],
+                'sp_ten' => $row['sp_ten'],
+                'sp_gia' => number_format($row['sp_gia'], 0, ".", ","),
+                'sp_giacu' => number_format($row['sp_giacu'], 0, ".", ","),
+                'sp_avt_tenfile' => $row['sp_avt_tenfile'],
+                'hsp_tenfile' => $row['hsp_tenfile'],
+                'sao' => $row['sao'] > 0 ? $row['sao'] : 0,
+            );
+        }
+        ?>
+        <h3 class="myfont text-danger mt-3 text-center">Sản phẩm giảm giá nhiều nhất</h3>
+        <div class="row row-cols-lg-4 row-cols-sm-3 row-cols-1">
+            <?php foreach ($dataDanhSachSanPhamGiamGia as $sp) : ?>
+                <div class="col py-3">
+                    <div class="card my-card">
+                        <a href="chitiet.php?sp_id=<?= $sp['sp_id'] ?>">
+                            <!-- Ảnh  -->
+                            <div class="my-box-card-img">
+                                <!-- Ảnh đại diện -->
+                                <?php if (!file_exists('../anh-do-an/' . $sp['sp_avt_tenfile'])) : ?>
+                                    <img src="/templatedoan/anh-do-an/default.png" alt="<?= $sp['sp_ten'] ?>" class="card-img-top my-card-img img-show">
+                                <?php else : ?>
+                                    <img src="/templatedoan/anh-do-an/<?= $sp['sp_avt_tenfile'] ?>" alt="<?= $sp['sp_ten'] ?>" class="card-img-top my-card-img img-show">
+                                <?php endif; ?>
+                                <!-- Ảnh thứ 2 -->
+                                <?php if (!file_exists('../anh-do-an/' . $sp['hsp_tenfile']) || empty($sp['hsp_tenfile'])) : ?>
+                                    <img src="/templatedoan/anh-do-an/default.png" alt="<?= $sp['sp_ten'] ?>" class="card-img-top my-card-img img-hide">
+                                <?php else : ?>
+                                    <img src="/templatedoan/anh-do-an/<?= $sp['hsp_tenfile'] ?>" alt="<?= $sp['sp_ten'] ?>" class="card-img-top my-card-img img-hide">
+                                <?php endif; ?>
+                                <!-- Đánh giá -->
+                                <div class="text-danger danh_gia">
+                                    <?php for ($i = 1; $i <= floor($sp['sao']); $i++) : ?>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                    <?php endfor; ?>
+                                    <?php for ($i = 1; $i <= ceil($sp['sao']) - floor($sp['sao']); $i++) : ?>
+                                        <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                                    <?php endfor; ?>
+                                    <?php for ($i = 1; $i <= 5 - ceil($sp['sao']); $i++) : ?>
+                                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                                    <?php endfor; ?>
+                                </div>
+                            </div>
+                        </a>
+                        <!-- Phần thân -->
+                        <div class="card-body px-0">
+                            <!-- Tên  -->
+                            <a href="chitiet.php?sp_id=<?= $sp['sp_id'] ?>" class="card-title my-card-title font-weight-bold">
+                                <?= $sp['sp_ten'] ?>
+                            </a>
+                            <!-- Giá  -->
+                            <h5 class="my-3">
+                                <?php if ($sp['sp_giacu'] != "0") : ?>
+                                    <span class="text-secondary"><s><?= $sp['sp_giacu'] ?></s>
+                                    <?php endif; ?>
+                                    </span> <span class="text-danger"><?= $sp['sp_gia'] ?> đ</span>
+                            </h5>
+                            <!-- Đặt mua  -->
                             <button class="btn myfont text-danger btn-add btn-mua" data-sp_id="<?= $sp['sp_id'] ?>">Thêm vào giỏ hàng</button>
                         </div>
                     </div>
@@ -277,6 +356,7 @@ include_once(__DIR__ . '/dbconnect.php');
             <?php endforeach; ?>
         </div>
     </div>
+
     <div id="themsp" style="display: none">
         <div class="shadow-lg p-3" id="a">
             <div class="row">
