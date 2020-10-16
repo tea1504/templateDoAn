a84aa1059a121f29b748a8c621b273599131d917
VyDuong162 <69305591+VyDuong162@users.noreply.github.com>
2020-10-16 19:40:53 +0700

    index

--- template-dashbroad/layouts/sidebar.php
+++ template-dashbroad/layouts/sidebar.php
@@ -6,7 +6,7 @@
         </div>
         <ul class="nav flex-column text-md-left text-center">
             <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link active" href="/templateDoAn/template-dashbroad/indexcopy.php">
                     <i class="fa fa-tachometer" aria-hidden="true"></i>
                     Dashboard
                 </a>
